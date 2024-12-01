1878b9f7ddb371fb807030c8cb3e507595b5e3e8
Alxndr32947 <alexsh32947@gmail.com>
2024-12-01 15:14:04 +0300

    Update module_1_3.py

--- module_1_3.py
+++ module_1_3.py
@@ -1,3 +1,4 @@
+# Практическая работа по уроку "Динамическая типизация":
 name = "Александр"
 print(name)
 
@@ -9,3 +10,11 @@ print(age)
 
 is_student = True
 print(is_student)
+
+# Практическое задание по теме "Переменные":
+course_name = 'Python'
+completed_tasks = 12
+hours_spent = 1.5
+time_for_one_task = float(hours_spent / completed_tasks)
+print("Курс: ", course_name, ", всего задач: ", str(completed_tasks), ", затрачено часов: ", str(hours_spent), \
+      ", среднее время выполнения ", str(time_for_one_task), " часа.")
